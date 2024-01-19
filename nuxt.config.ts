83831eac7819eb337348146e451bee8c1c9a61c7
justine6 <123348998+justine6@users.noreply.github.com>
2024-01-19 16:24:32 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,9 +10,9 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Justine Longla',
+      title: 'Longla Justine Tekang',
       meta: [
-        { name: 'description', content: 'A self-motivated Cloud Engineer offering about 7 years of experience in supporting and optimizing mission critical deployments in Azure and AWS. I design and implement CI/ CD pipeline using DevOps tools, provisioning, and managing highly available, fault-tolerant, and scalable infrastructures, utilizing terraform as IAC along with Ansible to automate configuration management. ' }
+        { name: 'description', content: 'A self-motivated Cloud Engineer years of experience in supporting and optimizing mission critical deployments in Azure and AWS. I design and implement CI/ CD pipeline using DevOps tools, provisioning, and managing highly available, fault-tolerant, and scalable infrastructures, utilizing terraform as IAC along with Ansible to automate configuration management. ' }
       ],
     }
   },
